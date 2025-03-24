74c7720ab02b085b514be69beffc345bc3bbdf25
Dickson Murithi <dicksonmurithi155@gmail.com>
2025-03-24 03:51:11 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // ✅ Use HashRouter
 
 import './index.css';
 import App from './App';
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* ✅ Change from BrowserRouter to HashRouter */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
